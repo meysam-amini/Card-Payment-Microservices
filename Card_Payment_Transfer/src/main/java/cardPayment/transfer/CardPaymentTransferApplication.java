c06ab8d9df27a1f84a00e41ad52d012be197fd5a
meysam-amini <meysamamini7394@gmail.com>
2022-03-31 02:06:38 +0430

    updated main class of transfer service

--- Card_Payment_Transfer/src/main/java/cardPayment/transfer/CardPaymentTransferApplication.java
+++ Card_Payment_Transfer/src/main/java/cardPayment/transfer/CardPaymentTransferApplication.java
@@ -6,12 +6,6 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.context.annotation.Bean;
-import org.springframework.data.redis.connection.ReactiveRedisConnectionFactory;
-import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
-import org.springframework.data.redis.core.ReactiveRedisTemplate;
-import org.springframework.data.redis.serializer.Jackson2JsonRedisSerializer;
-import org.springframework.data.redis.serializer.RedisSerializationContext;
-import org.springframework.data.redis.serializer.StringRedisSerializer;
 
 @SpringBootApplication
 @EnableDiscoveryClient
